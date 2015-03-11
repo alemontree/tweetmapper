2ea67a8eff257c38775b43d2f4eda6e8f7584dd6
Mikhail <mishnade@gmail.com>
2015-03-11 12:00:44 -0700

    Refractoring. Replaced print statemetns with python's logging, replaced threading with tweepy's Async Streaming option, changed hashtag as well as map zoom and centering.

--- tweet-mapper.py
+++ tweet-mapper.py
@@ -6,10 +6,11 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 import json
 import urllib2
-import threading
+import logging
 import config
 import traceback
 
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.WARNING)
 app = Flask(__name__, static_url_path='')
 app.debug = False
 app.config['SECRET_KEY'] = 'secret!'
@@ -26,11 +27,11 @@ def map():
 
 @socketio.on('message')
 def handle_message(message):
-    print 'message from client: ' + message
+    logging.info('message from client: %s', message)
 
 @socketio.on_error()
 def error_handler(e):
-    print 'error: ' + e
+    logging.error('error: %s', e) 
 
 
 class TweetListener(StreamListener):
@@ -44,29 +45,22 @@ class TweetListener(StreamListener):
 
     def on_data(self, data):
         try:
-            parsed_data = json.loads(data)
-            
+            parsed_data = json.loads(data)            
             coordinates = parsed_data["coordinates"]
-            # if coordinates:
-            #     print(parsed_data["text"])
-            #     print(parsed_data["user"]["name"])
-            #     print(parsed_data["coordinates"])
-            #     print()
             
             loc = parsed_data["user"]["location"]
             if not loc:
                 return True             
-            print(loc)
+            logging.info(loc)
             self.socketio.send(loc)
             return True
         except Exception as e:
-            print("Unexpected exception: ",)
-            print(traceback.format_exc())
+            logging.error("Unexpected exception: %s", 
+                traceback.format_exc())
             return True
 
     def on_exception(self, exception):
-        print("exception: " + str(exception))
-
+        logging.error("exception: %s", str(exception))
 
 
 consumer_key="y6avwjoMCJ1NwBEUXRVlK3kNW"
@@ -76,17 +70,11 @@ access_token="22721579-VjNiIU7xErbaADurQdS6VIwlBeMi6SzUlIT4j5R7A"
 access_token_secret="meHvXUBQWr3HLnTfVgkNPEXvzgtgJ8oyCmKPLYtaGKNVg"
 
 
-def start_stream(stream):
-    stream.filter(track=['#BlackLivesMatter']) # blocking
-
 if __name__ == "__main__":
     l = TweetListener(socketio)
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, l)
-    t = threading.Thread(target=start_stream, args=(stream,))
-    t.daemon = True
-
-    t.start()
+    stream.filter(track=['#android'], async=True) # blocking
 
     socketio.run(app, host=config.host, port=config.port)
